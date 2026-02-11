7664e60675451320365d7e6fad59473432720501
Tanvir Islam <YOUR_GITHUB_EMAIL>
2026-02-11 12:54:05 +0600

    remove metamask sdk

--- frontend/lib/wagmi.ts
+++ frontend/lib/wagmi.ts
@@ -1,13 +1,14 @@
 import { createConfig, http } from "wagmi";
 import { base } from "wagmi/chains";
-import { injected } from "wagmi/connectors";
+import { injected, walletConnect } from "wagmi/connectors";
 
 export const wagmiConfig = createConfig({
   chains: [base],
   connectors: [
-    injected({
-      shimDisconnect: true,
-    }),
+    injected(),
+    walletConnect({
+      projectId: "YOUR_WALLETCONNECT_PROJECT_ID"
+    })
   ],
   transports: {
     [base.id]: http(),
