870aacc97bd4efed1cd4d2ca96ed3f75a792feee
Tanvir Islam <YOUR_GITHUB_EMAIL>
2026-02-14 17:03:57 +0600

    fix farcaster authkit render API

--- frontend/components/FarcasterConnect.tsx
+++ frontend/components/FarcasterConnect.tsx
@@ -1,26 +1,24 @@
 "use client";
 
-import { SignInButton, useProfile } from "@farcaster/auth-kit";
+import { SignInButton } from "@farcaster/auth-kit";
 
 export default function FarcasterConnect({
   onSuccess,
 }: {
   onSuccess: () => void;
 }) {
-  const { profile } = useProfile();
-
-  if (profile) {
-    onSuccess();
-    return null;
-  }
-
   return (
-    <SignInButton>
-      <button className="connect-btn farcaster">
-        <img src="/farcaster.png" width={22} height={22} />
-        Connect Farcaster
-      </button>
-    </SignInButton>
+    <SignInButton
+      onSuccess={onSuccess}
+      render={({ signIn }) => (
+        <button
+          className="connect-btn farcaster"
+          onClick={() => signIn()}
+        >
+          <img src="/farcaster.png" width={22} height={22} />
+          Connect Farcaster
+        </button>
+      )}
+    />
   );
 }
-
