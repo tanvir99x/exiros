5cb913345b90e68771921fe84fb4b1a1c7678c79
Tanvir Islam <YOUR_GITHUB_EMAIL>
2026-02-14 21:33:18 +0600

    fix farcaster auth final correct API

--- frontend/components/FarcasterConnect.tsx
+++ frontend/components/FarcasterConnect.tsx
@@ -1,24 +1,25 @@
 "use client";
 
-import { SignInButton } from "@farcaster/auth-kit";
+import { useSignIn } from "@farcaster/auth-kit";
 
 export default function FarcasterConnect({
   onSuccess,
 }: {
   onSuccess: () => void;
 }) {
+  const { signIn, isSuccess } = useSignIn({
+    onSuccess: () => {
+      onSuccess();
+    },
+  });
+
   return (
-    <SignInButton
-      onSuccess={onSuccess}
-      render={({ signIn }) => (
-        <button
-          className="connect-btn farcaster"
-          onClick={() => signIn()}
-        >
-          <img src="/farcaster.png" width={22} height={22} />
-          Connect Farcaster
-        </button>
-      )}
-    />
+    <button
+      className="connect-btn farcaster"
+      onClick={() => signIn()}
+    >
+      <img src="/farcaster.png" width={22} height={22} />
+      Connect Farcaster
+    </button>
   );
 }
